125b05be0d6ba14481b1b564c5bf876b37e028c7
kaw3var@gmail.com <bat.zham@gmail.com>
2024-10-24 18:58:25 +0800

    added test

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -17,54 +17,64 @@ public class Main {
         List<CarModelDTO> allCars = fileSystemCarService.getAllCarDTOs(null);
 
         // Вывод всех машин
-//        if (allCars != null && !allCars.isEmpty()) {
-//            System.out.println("| ID     | Brand          | Model                               | Country of Origin               | Country Code  |");
-//            System.out.println("-------------------------------------------------------------------------------------------------------------");
-//
-//            for (CarModelDTO car : allCars) {
-//                System.out.println(car);
-//            }
-//        } else {
-//            System.out.println("Автомобили не найдены.");
-//        }
+        if (allCars != null && !allCars.isEmpty()) {
+            System.out.println("| ID     | Brand          | Model                               | Country of Origin               | Country Code  |");
+            System.out.println("-------------------------------------------------------------------------------------------------------------");
 
-//        // Все модели определенной марки
-//        List<CarModelDTO> toyotaCars = fileSystemCarService.getAllCarDTOs("Toyota");
-//        toyotaCars.forEach(System.out::println);
-//
-//        // Группировка моделей по марке
-//        fileSystemCarService.getCarModelGroupByModel("Toyota").forEach((model, count) ->
-//                System.out.println("Модель: " + model + ", Количество: " + count));
-//
-//        // Поиск автомобиля по объекту
-//        Optional<CarModelDTO> foundCar = fileSystemCarService.findCarById(71);
-//        foundCar.ifPresent(System.out::println);
+            for (CarModelDTO car : allCars) {
+                System.out.println(car);
+            }
+        } else {
+            System.out.println("Автомобили не найдены.");
+        }
 
-        // Создаем список автомобилей
-//        List<CarModelDTO> carList = Arrays.asList(
-//                new CarModelDTO(1, "Mercedes", "GLE 400", "Германия", "DE"),
-//                new CarModelDTO(2, "BMW", "1 Series", "Германия", "DE"),
-//                new CarModelDTO(3, "BMW", "X5", "Германия", "DE"),
-//                new CarModelDTO(4, "FSO", "Polonez", "Польша", "PL"),
-//                new CarModelDTO(5, "Audi", "A4", "Германия", "DE")
-//        );
-//
-//        CarService carService = new CarService(carList);
-//
-//        // Получение уникальных марок автомобилей
-//        Set<String> uniqueBrands = carService.getUniqueBrands();
-//        System.out.println("Уникальные марки автомобилей:");
-//        uniqueBrands.forEach(System.out::println);
-//
-//        // Поиск моделей по марке автомобиля
-//        String brandToSearch = "BMW";
-//        List<CarModelDTO> bmwModels = carService.getModelsByBrand(brandToSearch);
-//        System.out.println("\nМодели автомобилей марки " + brandToSearch + ":");
-//        bmwModels.forEach(car -> System.out.println(car.getModel()));
-//
-//        // Группировка по марке автомобиля
-//        Map<String, Integer> brandCountMap = carService.groupByBrand();
-//        System.out.println("\nГруппировка автомобилей по маркам:");
-//        brandCountMap.forEach((brand, count) -> System.out.println(brand + ": " + count));
+         // Все модели определенной марки
+        List<CarModelDTO> toyotaCars = fileSystemCarService.getAllCarDTOs("Toyota");
+        toyotaCars.forEach(System.out::println);
+
+        // Группировка моделей по марке
+        fileSystemCarService.getCarModelGroupByModel("Toyota").forEach((model, count) ->
+                System.out.println("Модель: " + model + ", Количество: " + count));
+
+        // Поиск автомобиля по объекту
+        Optional<CarModelDTO> foundCar = fileSystemCarService.findCarById(71);
+        foundCar.ifPresent(System.out::println);
+
+         // Создаем список автомобилей
+        List<CarModelDTO> carList = Arrays.asList(
+                new CarModelDTO(1, "Mercedes", "GLE 400", "Германия", "DE"),
+                new CarModelDTO(2, "BMW", "1 Series", "Германия", "DE"),
+                new CarModelDTO(3, "BMW", "X3", "Германия", "DE"),
+                new CarModelDTO(4, "BMW", "X4", "Германия", "DE"),
+                new CarModelDTO(5, "BMW", "X5", "Германия", "DE"),
+                new CarModelDTO(6, "BMW", "X6", "Германия", "DE"),
+                new CarModelDTO(7, "FSO", "Polonez", "Польша", "PL"),
+                new CarModelDTO(8, "Mazda", "Verisa", "Япония", "JP"),
+                new CarModelDTO(9, "Mazda", "MX-30", "Япония", "JP"),
+                new CarModelDTO(10, "Mazda", "MX-5", "Япония", "JP"),
+                new CarModelDTO(11, "Audi", "A4", "Германия", "DE"),
+                new CarModelDTO(12, "Audi", "Q3", "Германия", "DE"),
+                new CarModelDTO(13, "Mercedes", "CLC 280", "Германия", "DE"),
+                new CarModelDTO(14, "Mercedes", "GLE 500", "Германия", "DE"),
+                new CarModelDTO(15, "Audi", "Q7", "Германия", "DE")
+        );
+
+        CarService carService = new CarService(carList);
+
+        // Получение уникальных марок автомобилей
+        Set<String> uniqueBrands = carService.getUniqueBrands();
+        System.out.println("Уникальные марки автомобилей:");
+        uniqueBrands.forEach(System.out::println);
+
+        // Поиск моделей по марке автомобиля
+        String brandToSearch = "BMW";
+        List<CarModelDTO> bmwModels = carService.getModelsByBrand(brandToSearch);
+        System.out.println("\nМодели автомобилей марки " + brandToSearch + ":");
+        bmwModels.forEach(car -> System.out.println(car.getModel()));
+
+        // Группировка по марке автомобиля
+        Map<String, Integer> brandCountMap = carService.groupByBrand();
+        System.out.println("\nГруппировка автомобилей по маркам:");
+        brandCountMap.forEach((brand, count) -> System.out.println(brand + ": " + count));
     }
 }
